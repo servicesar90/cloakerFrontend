d812b29de4bc54c605df83862dd8da5e5900192d
servicesar90 <servicesar90@gmail.com>
2025-11-23 22:07:45 -0800

    landing page add

--- src/api/Apis.js
+++ src/api/Apis.js
@@ -1,8 +1,8 @@
-const base_url = 'http://localhost:2000';
+// const base_url = 'http://localhost:2000';
 // // const base_url="http://192.168.1.12:2000"
 // const base_url="http://192.168.1.3:2000";
 
-// const base_url = "https://app.clockerly.io"
+const base_url = "https://app.clockerly.io"
 
 // const base_url= "https://app.clockerly.io"
 
